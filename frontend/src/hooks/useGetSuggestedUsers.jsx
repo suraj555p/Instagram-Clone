05f5a9a844f78251decb8203711adf6855fe65bf
suraj <suraj87parmar@gmail.com>
2024-11-25 10:09:54 +0530

    url

--- frontend/src/hooks/useGetSuggestedUsers.jsx
+++ frontend/src/hooks/useGetSuggestedUsers.jsx
@@ -9,7 +9,7 @@ const useGetSuggestedUsers = () => {
     useEffect(() => {
         const fetchSuggestedUsers = async () => {
             try {
-                const res = await axios.get('https://instaclone-g9h5.onrender.com/api/v1/user/suggested', { withCredentials: true });
+                const res = await axios.get('https://instagram-clone-sdnf.onrender.com/api/v1/user/suggested', { withCredentials: true });
                 if (res.data.success) { 
                     dispatch(setSuggestedUsers(res.data.users));
                 }
