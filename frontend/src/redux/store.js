57ce3e9012dfcded3874630331286b6d69095782
suraj <suraj87parmar@gmail.com>
2024-11-25 10:48:11 +0530

    gsk3

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -38,7 +38,7 @@ const store = configureStore({
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
             serializableCheck: {
-                ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+                ignoredActions: [PERSIST,FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
             },
         }),
 });
